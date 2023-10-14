645f23ec990333d62ee47eb4434d1525f1f5d6ae
revanite <evgkol69@gmail.com>
2023-10-14 09:01:55 -0400

    Task 1

---
+++ src/main/java/edu/hw2/Expr.java
@@ -0,0 +1,41 @@
+package edu.hw2;
+
+public sealed interface Expr {
+	double evaluate();
+
+	record Constant(double value) implements Expr {
+		public double evaluate() {
+			return value;
+		}
+	}
+
+	public record Negate(Expr exp) implements Expr {
+		public double evaluate() {
+			return -(exp.evaluate());
+		}
+	}
+
+	record Exponent(Expr exp, int n) implements Expr {
+		public double evaluate() {
+			return exponent(exp.evaluate(), n);
+		}
+
+		private double exponent(double value, int n) {
+			if (n == 0) {
+				return 1;
+			}
+			return value * exponent(value, n - 1);
+		}
+	}
+
+	record Addition(Expr left, Expr right) implements Expr {
+		public double evaluate() {
+			return left.evaluate() + right.evaluate();
+		}
+	}
+	record Multiplication(Expr left, Expr right) implements Expr {
+		public double evaluate() {
+			return left.evaluate() * right.evaluate();
+		}
+	}
+}
