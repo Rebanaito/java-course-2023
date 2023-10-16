4af1ea4d8b4ccd938e5fcea535168bbda6818656
revanite <evgkol69@gmail.com>
2023-10-16 06:22:23 -0400

    Task 4 tests

--- src/main/java/edu/hw2/Info.java
+++ src/main/java/edu/hw2/Info.java
@@ -4,13 +4,8 @@ public class Info {
 	public record CallingInfo(String className, String methodName) {
 	}
 
-	public static CallingInfo callingInfo(Throwable info) {
-		var stack = info.getStackTrace();
-		return new CallingInfo(stack[stack.length-1].getClassName(), stack[stack.length-1].getMethodName());
-	}
-
 	public static CallingInfo callingInfo() {
 		var stack = Thread.currentThread().getStackTrace();
-		return new CallingInfo(stack[stack.length-1].getClassName(), stack[stack.length-1].getMethodName());
+		return new CallingInfo(stack[2].getClassName(), stack[2].getMethodName());
 	}
 }
--- src/main/java/edu/hw2/Main.java
+++ src/main/java/edu/hw2/Main.java
@@ -2,7 +2,7 @@ package edu.hw2;
 
 public class Main {
 	public static void main(String[] args) {
-		var info = Info.callingInfo(new Throwable());
+		var info = Info.callingInfo();
 		System.out.println(info.className());
 		System.out.println(info.methodName());
 	}
