7aab398907083b7b3fb21b9ad3342324788fce7d
revanite <evgkol69@gmail.com>
2023-10-17 15:58:02 -0400

    Reformatted for the style check

--- src/main/java/edu/project1/Session.java
+++ src/main/java/edu/project1/Session.java
@@ -4,85 +4,86 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
+@SuppressWarnings("RegexpSinglelineJava")
 public class Session {
-	public Session() {
-		Random rand = new Random();
-		int index = rand.nextInt(dictionary.length);
-		this.answer = new Word(dictionary[index]);
-		this.lettersLeft = dictionary[index].length();
-	}
+    public Session() {
+        Random rand = new Random();
+        int index = rand.nextInt(DICTIONARY.length);
+        this.answer = new Word(DICTIONARY[index]);
+        this.lettersLeft = DICTIONARY[index].length();
+    }
 
-	public Session(String[] words) {
-		Random rand = new Random();
-		int index = rand.nextInt(words.length);
-		this.answer = new Word(words[index]);
-		this.lettersLeft = words[index].length();
-	}
+    public Session(String[] words) {
+        Random rand = new Random();
+        int index = rand.nextInt(words.length);
+        this.answer = new Word(words[index]);
+        this.lettersLeft = words[index].length();
+    }
 
-	private final static Logger LOGGER = Logger.getLogger("LOGGER");
-	private final static int MAX_MISTAKES = 5;
-	private final static int LONGEST_WORD = 45;
-	private final static String[] dictionary = {"prandial", "jocundity", "superfluous", "exuberant"};
-	private final Word answer;
-	private int lettersLeft;
-	private int mistakesMade = 0;
+    private final static Logger LOGGER = Logger.getLogger("LOGGER");
+    private final static int MAX_MISTAKES = 5;
+    private final static int LONGEST_WORD = 45;
+    private final static String[] DICTIONARY = {"prandial", "jocundity", "superfluous", "exuberant"};
+    private final Word answer;
+    private int lettersLeft;
+    private int mistakesMade = 0;
 
-	int play() {
-		if (gameOver()) {
-			result();
-			return 1;
-		}
-		Scanner scanner = new Scanner(System.in);
-		if (answer.length() == 0 || answer.length() > LONGEST_WORD) {
-			LOGGER.info("Invalid word");
-			return 1;
-		}
-		while (!gameOver()) {
-			System.out.println("Guess a letter:");
-			String in = scanner.nextLine();
-			tryInput(in);
-		}
-		result();
-		return 0;
-	}
+    int play() {
+        if (gameOver()) {
+            result();
+            return 1;
+        }
+        Scanner scanner = new Scanner(System.in);
+        if (answer.length() == 0 || answer.length() > LONGEST_WORD) {
+            LOGGER.info("Invalid word");
+            return 1;
+        }
+        while (!gameOver()) {
+            System.out.println("Guess a letter:");
+            String in = scanner.nextLine();
+            tryInput(in);
+        }
+        result();
+        return 0;
+    }
 
-	boolean gameOver() {
-		return haveLost() || haveWon();
-	}
+    boolean gameOver() {
+        return haveLost() || haveWon();
+    }
 
-	boolean haveWon() {
-		return lettersLeft == 0;
-	}
+    boolean haveWon() {
+        return lettersLeft == 0;
+    }
 
-	boolean haveLost() {
-		return mistakesMade == MAX_MISTAKES;
-	}
+    boolean haveLost() {
+        return mistakesMade == MAX_MISTAKES;
+    }
 
-	private void result() {
-		if (haveWon()) {
-			System.out.println("You won!");
-		} else {
-			System.out.println("You lost!");
-		}
-	}
+    private void result() {
+        if (haveWon()) {
+            System.out.println("You won!");
+        } else {
+            System.out.println("You lost!");
+        }
+    }
 
-	void tryInput(String in) {
-		if (in.length() != 1) {
-			return;
-		}
-		int opened = answer.tryLetter(in.charAt(0));
-		if (opened == 0) {
-			System.out.println("Miss!\n");
-			mistakesMade++;
-		} else {
-			System.out.println("Hit!\n");
-			lettersLeft -= opened;
-		}
-		answer.printGuessed();
-	}
+    void tryInput(String in) {
+        if (in.length() != 1) {
+            return;
+        }
+        int opened = answer.tryLetter(in.charAt(0));
+        if (opened == 0) {
+            mistakesMade++;
+            System.out.printf("Miss %d out of %d!\n", mistakesMade, MAX_MISTAKES);
+        } else {
+            System.out.println("Hit!\n");
+            lettersLeft -= opened;
+        }
+        answer.printGuessed();
+    }
 
-	public static void main(String[] args) {
-		var sesh = new Session();
-		sesh.play();
-	}
+//    public static void main(String[] args) {
+//        var sesh = new Session();
+//        sesh.play();
+//    }
 }
--- src/main/java/edu/project1/Word.java
+++ src/main/java/edu/project1/Word.java
@@ -1,44 +1,45 @@
 package edu.project1;
 
+@SuppressWarnings("RegexpSinglelineJava")
 public class Word {
-	public Word(String word) {
-		this.word = word;
-		this.chars = word.toCharArray();
-		this.guessed = new boolean[this.chars.length];
-	}
+    public Word(String word) {
+        this.word = word;
+        this.chars = word.toCharArray();
+        this.guessed = new boolean[this.chars.length];
+    }
 
-	private final String word;
-	private final char[] chars;
-	private final boolean[] guessed;
+    private final String word;
+    private final char[] chars;
+    private final boolean[] guessed;
 
-	public String getWord() {
-		return this.word;
-	}
+    public String getWord() {
+        return this.word;
+    }
 
-	public int tryLetter(char c) {
-		int count = 0;
-		for (int i = 0; i < chars.length; i++) {
-			if (chars[i] == c && !guessed[i]) {
-				count++;
-				guessed[i] = true;
-			}
-		}
-		return count;
-	}
+    public int tryLetter(char c) {
+        int count = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == c && !guessed[i]) {
+                count++;
+                guessed[i] = true;
+            }
+        }
+        return count;
+    }
 
-	public int length() {
-		return this.chars.length;
-	}
+    public int length() {
+        return this.chars.length;
+    }
 
-	public void printGuessed() {
-		System.out.print("The word: ");
-		for (int i = 0; i < chars.length; i++) {
-			if (guessed[i]) {
-				System.out.print(chars[i]);
-			} else {
-				System.out.print("*");
-			}
-		}
-		System.out.println();
-	}
+    public void printGuessed() {
+        System.out.print("The word: ");
+        for (int i = 0; i < chars.length; i++) {
+            if (guessed[i]) {
+                System.out.print(chars[i]);
+            } else {
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+    }
 }
