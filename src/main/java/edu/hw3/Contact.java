acc89082c31943149e487cb18c81964362079f08
revanite <evgkol69@gmail.com>
2023-10-21 11:06:17 -0400

    Task 5

---
+++ src/main/java/edu/hw3/Contact.java
@@ -0,0 +1,46 @@
+package edu.hw3;
+
+import java.util.Arrays;
+
+public record Contact(String firstName, String lastName) {
+	public static Contact[] parseContacts(String[] contacts, String order) {
+		if (invalidArguments(contacts, order)) {
+			return new Contact[0];
+		}
+
+		for (int i = 0; i < contacts.length; i++) {
+			var split = contacts[i].split(" ");
+			if (split.length != 2) {
+				return new Contact[0];
+			}
+			contacts[i] = split[1] + " " + split[0];
+		}
+		Arrays.sort(contacts);
+
+		return makeList(contacts, order);
+	}
+
+	private static boolean invalidArguments(String[] contacts, String order) {
+		if (contacts == null || order == null) {
+			return true;
+		}
+		return (!order.equals("ASC") && !order.equals("DESC"));
+	}
+
+	private static Contact[] makeList(String[] contactsReversed, String order) {
+		var contactList = new Contact[contactsReversed.length];
+
+		if (order.equals("ASC")) {
+			for (int j = 0; j < contactsReversed.length; j++) {
+				var split = contactsReversed[j].split(" ");
+				contactList[j] = new Contact(split[1], split[0]);
+			}
+		} else {
+			for (int j = 0; j < contactsReversed.length; j++) {
+				var split = contactsReversed[contactsReversed.length-1-j].split(" ");
+				contactList[j] = new Contact(split[1], split[0]);
+			}
+		}
+		return contactList;
+	}
+}
---
+++ src/test/java/edu/hw3/Task5Test.java
@@ -0,0 +1,55 @@
+package edu.hw3;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Task5Test {
+	@Test
+	@DisplayName("Valid contact list test 1")
+	void validList1() {
+		var list = Contact.parseContacts(new String[] {"John Locke", "Thomas Aquinas", "David Hume", "Rene Descartes"}, "ASC");
+		assertThat(list).isNotNull();
+		assertThat(list.length).isNotEqualTo(0);
+		assertThat(list[0].firstName() + " " + list[0].lastName()).isEqualTo("Thomas Aquinas");
+		assertThat(list[1].firstName() + " " + list[1].lastName()).isEqualTo("Rene Descartes");
+		assertThat(list[2].firstName() + " " + list[2].lastName()).isEqualTo("David Hume");
+		assertThat(list[3].firstName() + " " + list[3].lastName()).isEqualTo("John Locke");
+	}
+
+	@Test
+	@DisplayName("Valid contact list test 2")
+	void validList2() {
+		var list = Contact.parseContacts(new String[] {"Paul Erdos", "Leonhard Euler", "Carl Gauss"}, "DESC");
+		assertThat(list).isNotNull();
+		assertThat(list.length).isNotEqualTo(0);
+		assertThat(list[0].firstName() + " " + list[0].lastName()).isEqualTo("Carl Gauss");
+		assertThat(list[1].firstName() + " " + list[1].lastName()).isEqualTo("Leonhard Euler");
+		assertThat(list[2].firstName() + " " + list[2].lastName()).isEqualTo("Paul Erdos");
+	}
+
+	@Test
+	@DisplayName("Invalid list 1")
+	void invalidList1() {
+		var list = Contact.parseContacts(new String[] {}, "DESC");
+		assertThat(list).isNotNull();
+		assertThat(list.length).isEqualTo(0);
+	}
+
+	@Test
+	@DisplayName("Invalid list 2")
+	void invalidList2() {
+		var list = Contact.parseContacts(null, "DESC");
+		assertThat(list).isNotNull();
+		assertThat(list.length).isEqualTo(0);
+	}
+
+	@Test
+	@DisplayName("Invalid list 3")
+	void invalidList3() {
+		var list = Contact.parseContacts(new String[] {"Joe", "Matt", "Axel"}, "ASC");
+		assertThat(list).isNotNull();
+		assertThat(list.length).isEqualTo(0);
+	}
+}
