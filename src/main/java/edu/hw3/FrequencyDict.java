a4b01d3f70d3d7c1cacffeb459c05d9d10a5cc96
revanite <evgkol69@gmail.com>
2023-10-30 08:51:54 -0400

    Simplified frequency dict with HashMap.getOrDefault

--- src/main/java/edu/hw3/FrequencyDict.java
+++ src/main/java/edu/hw3/FrequencyDict.java
@@ -9,12 +9,7 @@ public class FrequencyDict {
     public static <T> Hashtable<T, Integer> freqDict(T[] data) {
         Hashtable<T, Integer> table = new Hashtable<>();
         for (int i = 0; i < data.length; i++) {
-            Integer value = table.get(data[i]);
-            if (value == null) {
-                table.put(data[i], 1);
-            } else {
-                table.put(data[i], value + 1);
-            }
+            table.put(data[i], table.getOrDefault(data[i], 0) + 1);
         }
         return table;
     }
