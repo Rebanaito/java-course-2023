068578dfbc175a36b44d43f2ab261eb3070da129
revanite <evgkol69@gmail.com>
2023-11-05 03:42:06 -0500

    Task 6

---
+++ src/main/java/edu/hw5/Task6.java
@@ -0,0 +1,12 @@
+package edu.hw5;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task6 {
+	public static boolean contains(String string, String substring) {
+		Pattern pattern = Pattern.compile(".*" + substring + ".*");
+		Matcher matcher = pattern.matcher(string);
+		return matcher.find();
+	}
+}
