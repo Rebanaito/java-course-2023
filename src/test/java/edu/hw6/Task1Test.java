f75f445db165bbff17ab83f6378a95198d9f8a2b
revanite <evgkol69@gmail.com>
2023-11-17 13:54:24 -0500

    Task 2

--- src/test/java/edu/hw6/Task1Test.java
+++ src/test/java/edu/hw6/Task1Test.java
@@ -1,6 +1,5 @@
 package edu.hw6;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
